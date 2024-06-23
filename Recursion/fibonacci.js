d84b3d8c22f9e2e31b10b72eb022449587909688
Akhil Nagpal <akhilnagpal07@gmail.com>
2024-06-24 00:41:39 +0530

    Implemented Fibonacci

---
+++ Recursion/fibonacci.js
@@ -0,0 +1,27 @@
+// *-*-*-*-*-* FIBONACCI SEQUENCE *-*-*-*-*-*
+
+// *-*-*-*-* Iterative Approach *-*-*-*-*
+function fibonacci(value) {
+  let n0 = 0;
+  let n1 = 1;
+
+  for (let i = 0; i < value; i++) {
+    temp = n0 + n1;
+    n0 = n1;
+    n1 = temp;
+  }
+  return n0;
+}
+console.log(fibonacci(6));
+
+// *-*-*-*-* Recursive Approach *-*-*-*-*
+function recursiveFibonacci(value) {
+  // Base Case -> if the value is less then or equalls to 1
+  if (value <= 1) {
+    return value;
+  }
+
+  return (
+    recursiveFibonacci(value - 2) + recursiveFibonacci(value - 1)
+  );
+}
